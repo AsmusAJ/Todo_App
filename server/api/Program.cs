b391c0a9c11e5eb9a414e23d3be827ef1d01c28f
asmusaj <asmusaj@umich.edu>
2026-05-27 23:07:37 -0400

    fix

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,30 @@
+using efscaffold.Entities;
+using Infrastructure.Postgres.Scaffolding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql();
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices]MyDbContext dbContext) => 
+{
+    var temp = new Todo()
+    {
+        Description = "test",
+        Title = "test title",
+        Id = Guid.NewGuid().ToString(),
+        Priority = 5
+    };
+    dbContext.Todos.Add(temp);
+    dbContext.SaveChanges();
+    var objects = dbContext.Todos.ToList();
+    return objects;
+});
 
 app.Run();
